93fe25cad3430657cfab35dd5cdb99d52621801c
Idan Dardikman <idandrd@gmail.com>
2019-04-06 15:41:19 +0300

    added material ui (#2)

--- src/SaveFile.tsx
+++ src/SaveFile.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { saveAs } from "file-saver";
+import Button from "@material-ui/core/Button";
 
 function export2Doc(elementId: string, filename = "") {
   const elemant = document.getElementById(elementId);
@@ -28,22 +29,16 @@ export class SaveFile extends Component<{ elementId: string }> {
   render() {
     const fileName = "הסכם שכירות בלתי מוגנת";
     return (
-      <div style={{display: "flex", justifyContent: "center", marginTop: 50}}>
-        <button
-          style={{
-            backgroundColor: "#F3A8E4",
-            borderColor: "#F3A8E4",
-            fontWeight: 900,
-            borderRadius: 20,
-            padding: 10,
-            fontSize: 20,
-            cursor: "pointer",
-            width: 300
-          }}
+      <div style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{width: 300}}
+          size="large"
           onClick={() => export2Doc("someRandomId", fileName)}
         >
           שמור מסמך
-        </button>
+        </Button>
       </div>
     );
   }
